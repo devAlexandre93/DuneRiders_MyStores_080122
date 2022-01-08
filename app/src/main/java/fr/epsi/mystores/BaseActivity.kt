c861ee992c59a2e611bf50306629e10f29431a66
aanan <anania.alexandre@gmail.com>
2022-01-08 20:25:40 +0100

    Add header and custom colors

--- app/src/main/java/fr/epsi/mystores/BaseActivity.kt
+++ app/src/main/java/fr/epsi/mystores/BaseActivity.kt
@@ -1,7 +1,22 @@
 package fr.epsi.mystores
 
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 open class BaseActivity : AppCompatActivity() {
 
+    fun showBack() {
+        val imageViewBack = findViewById<ImageView>(R.id.imageViewBack)
+        imageViewBack.visibility = View.VISIBLE
+        imageViewBack.setOnClickListener(View.OnClickListener {
+            onBackPressed()
+        })
+    }
+
+    fun setHeaderTitle(text: String) {
+        val textViewTitle = findViewById<TextView>(R.id.textViewTitle)
+        textViewTitle.text = text
+    }
 }
